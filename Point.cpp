63a4132e5c7f76a2823ee1bc001097faa87069b5
BryanWheeler <Bryan.C.Wheeler@ucdenver.edu>
2015-09-07 20:37:55 -0600

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -1,17 +1,21 @@
 #include "Point.h"
+#include <cmath>
+
 
 // Default constructor
 // Initializes the point to (0.0, 0.0)
 Point::Point() {
   x = 0.0;
   y = 0.0;
+  z = 0.0;
 }
 
 // Constructor
 // Initializes the point to (initX, initY)
-Point::Point(double initX, double initY) {
+Point::Point(double initX, double initY, double initZ) {
   x = initX;
   y = initY;
+  z = initZ;
 }
 
 // Destructor
@@ -31,6 +35,10 @@ void Point::setY(double newY) {
   y = newY;
 }
 
+void Point::setZ(double newZ) {
+  z = newZ;
+}
+
 // Accessors
 // Return the current values of private member variables
 
@@ -41,3 +49,18 @@ double Point::getX() {
 double Point::getY() {
   return y;
 }
+
+double Point::getZ() {
+  return z;
+}
+
+//Functions
+double Point::distanceTo(Point& point) {
+  double distance = 0.0;
+  distance = sqrt(pow(point.x - getX(), 2) + pow(point.y - getY(), 2) + pow(point.z - getZ(), 2));
+
+  return distance;
+
+}
+
+
